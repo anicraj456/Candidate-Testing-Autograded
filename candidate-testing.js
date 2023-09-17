991c0186f5257c670676a0f07060282fbd9ab0ac
anicraj456 <anicraj123@gmail.com>
2023-09-17 10:32:37 -0500

    candidate testing completed

--- candidate-testing.js
+++ candidate-testing.js
@@ -7,18 +7,18 @@ let candidateName = "";
 
 
 // TODO 1.2a: Define question, correctAnswer, and candidateAnswer //
+
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
 
 
 //TODO: Variables for Part 2
-let questions =  ['Who was the first American woman in space? ','True or false: 5 kilometer == 5000 meters? ','(5 + 3)/2 * 10 = ? ','Given the array [8, Orbit, Trajectory, 45] what entry is at index 2? ' , 'What is the minimum crew size for the ISS? '];
-let correctAnswers = ['Sally Ride' , 'true' , 40 ,'Trajectory' , 3 ];
-let candidateAnswers = "";
+let questions =  ['Who was the first American woman in space? ','True or false: 5 kilometer == 5000 meters? ','(5 + 3)/2 * 10 = ? ','Given the array [8, \'Orbit\', \'Trajectory\', 45], what entry is at index 2? ' , 'What is the minimum crew size for the ISS? '];
+let correctAnswers = ["Sally Ride" , "true" , "40" ,"Trajectory" , "3" ];
+let candidateAnswers = [];
 
-console.log(questions);
-
+//runProgram();
 
 function askForName() {
   // TODO 1.1b: Ask for candidate's name //
@@ -28,65 +28,51 @@ function askForName() {
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
 
-  console.log(questions[0]);
-  candidateAnswers = input.question("Enter your Answer: ");
-  /*if (candidateAnswers == correctAnswers[0] ){
-    console.log("correct answer");
-  } else {
-    console.log("wrong answer");
-  }*/
-
-  console.log(questions[1]);
-  candidateAnswers = input.question("Enter your Answer: ");
-  if (candidateAnswers == correctAnswers[1] ){
-    console.log("correct answer");
-  } else {
-    console.log("wrong answer");
+  let i;
+  for(i=0;i<questions.length;i++){
+    console.log(questions[i]);
+    candidateAnswers[i] = input.question("Enter your answer-------");
+    console.log(candidateAnswers[i]);
   }
-
-  console.log(questions[2]);
-  candidateAnswers = input.question("Enter your Answer: ");
-  if (candidateAnswers == correctAnswers[2] ){
-    console.log("correct answer");
-  } else {
-    console.log("wrong answer");
-  }
-
-  console.log(questions[3]);
-  candidateAnswers = input.question("Enter your Answer: ");
-  if (candidateAnswers == correctAnswers[3] ){
-    console.log("correct answer");
-  } else {
-    console.log("wrong answer");
-  }
-
-  console.log(questions[4]);
-  candidateAnswers = input.question("Enter your Answer: ");
-  if (candidateAnswers == correctAnswers[4] ){
-    console.log("correct answer");
-  } else {
-    console.log("wrong answer");
-  }
-
-  console.log(question);
-  candidateAnswers = input.question("Enter your Answer: ");
-
+  console.log("answer array "+candidateAnswers);
 }
 
 function gradeQuiz(candidateAnswers) {
 
   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+  let score = 0;
+  console.log(candidateAnswers);
+  let j;
+  for (j=0;j<candidateAnswers.length;j++){
+    console.log(candidateAnswers[j]);
+    console.log(correctAnswers[j]);
+    if(candidateAnswers[j].toUpperCase() === correctAnswers[j].toUpperCase() ){
+      console.log("correct answer");
+      score++
+    } else{ 
+      console.log("wrong answer");
+    }
+    }
 
- if (candidateAnswers == correctAnswer){
-   console.log("Correct Answer!");
- } else{
-  console.log("Wrong Answer");
+    //TODO 3.2 use this variable to calculate the candidates score.
+ //console.log("The total grade is "+grade);
+ let grade = 0;
+
+ console.log("grade "+ score);
+ console.log(questions.length);
+ 
+ grade = (score / questions.length) * 100 ;
+
+ console.log("Grade percentage   "+ grade);
+ 
+  if(grade >= 80){
+   console.log("you have passed");
+ }else  {
+   console.log("you have failed");
  }
-
-  let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-  return grade;
-}
+    console.log("Total number of correct answers   "+ score );
+    return grade;
+  }
 
 function runProgram() {
   askForName();
